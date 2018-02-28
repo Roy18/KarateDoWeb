ac1a27eb12c4dfd1c4ca0e4a33c29a2ae3811980
erickmelon <35412685+erickmelon@users.noreply.github.com>
2018-02-27 22:32:38 -0600

    revisar

---
+++ Karate/src/com/glitched/Karatewebprofile/actions/DeleteVideoAction.java
@@ -0,0 +1,49 @@
+package com.glitched.Karatewebprofile.actions;
+
+import com.glitched.Karatewebprofile.models.Post;
+import com.glitched.Karatewebprofile.models.KarateService;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeleteVideoAction extends ActionSupport implements SessionAware {
+    private Map<String, Object> sessionMap;
+    private List<Post> posts;
+    int postId;
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    public String execute() throws Exception {
+        KarateService service = new KarateService();
+        if (sessionMap.containsKey("username")) {
+            if (service.deleteCommentByPost(postId)) {
+                if(service.deletePostById(postId)) {
+                    setPosts(service.findByUser(postId));
+                    return SUCCESS;
+                }
+            }
+        }
+        return ERROR;
+    }
+}
